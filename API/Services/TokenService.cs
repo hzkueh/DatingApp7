309c3d884c6998687005125071548c4f31082def
KuehHZ <37015145+hzkueh@users.noreply.github.com>
2023-02-09 08:43:03 +0800

    End of section 5

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -16,7 +16,7 @@ namespace API.Services
     {
         private readonly SymmetricSecurityKey _key;
         public TokenService(IConfiguration config){
-            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["ToknKey"]));
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
         }
         public string CreateToken(AppUser user)
         {
